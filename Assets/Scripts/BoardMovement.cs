23667c06ab80bde91c300b388534df27aff0ce1f
Juno AM <sabotaione@gmail.com>
2024-02-08 18:27:31 -0500

    copied over project

---
+++ Assets/Scripts/BoardMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMovement : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            Debug.Log("pressed the w key");
+            transform.position += new Vector3(0f, 0f, 1f);
+        }
+    }
+}
